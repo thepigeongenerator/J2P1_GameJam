546e78e3d622bdb1cbe2c9ef1be0687618403c86
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-08-27 23:25:01 +0200

    make plate spawning + edit plate sprite behaviour

--- Assets/scripts/Plate.cs
+++ Assets/scripts/Plate.cs
@@ -2,19 +2,17 @@ using System;
 using UnityEngine;
 public class Plate : MonoBehaviour
 {
-    // load in the plate sprites in the order from empty to full
-    [SerializeField] private Sprite[] sprites;
-    private SpriteRenderer render;
-    private Table table;
-    private int plateFullness; // index of the sprite + indicator of how full the plate is
+    private bool isEmpty = false;
     private float radius;
+    public GameObject food;
+    public Table table;
 
 
     // called when the mouse is held or clicked
     private void OnMouse(bool isHeld)
     {
         // if the plate is empty, move the plate to the mouse's position
-        if (plateFullness == 0)
+        if (isEmpty == true)
         {
             transform.position = Input.mousePosition;
             return;
@@ -24,8 +22,8 @@ public class Plate : MonoBehaviour
         if (isHeld) return;
 
         //otherwise decrease the fullness and update the sprite
-        plateFullness--;
-        render.sprite = sprites[plateFullness];
+        isEmpty = true;
+        Destroy(food);
     }
 
     // checks whether a position falls on the plate
@@ -47,24 +45,24 @@ public class Plate : MonoBehaviour
     // when the object is loaded
     private void Awake()
     {
-        table = FindObjectOfType<Table>();
-        render = GetComponent<SpriteRenderer>();
-        plateFullness = sprites.Length - 1; // set fullness to full
+        SpriteRenderer render = GetComponent<SpriteRenderer>();
 
         // update the renderer and calculate the radius (assuming all sprites are the same size)
-        render.sprite = render.sprite = sprites[plateFullness];
         radius = render.size.y / 2.0F;
     }
 
     // called on every frame upate
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && IsOnPlate(Input.mousePosition))
         {
             bool isHeld = Input.GetMouseButtonDown(0) == false;
             OnMouse(isHeld);
         }
-        else if (table.IsOnTable(transform.position)) {}
+        else if (table.IsOnTable(transform.position) == false)
+        {
+            table.RemovePlate(this);
+        }
     }
 
     // draws the hitbox outline in the editor when the object is selected
--- Assets/scripts/Table.cs
+++ Assets/scripts/Table.cs
@@ -1,10 +1,41 @@
 using UnityEngine;
 public class Table : MonoBehaviour
 {
+    [SerializeField] private Sprite plateSprite;
+    [SerializeField] private Sprite[] foodSprites;
+    private int plateCount = 0;
+
+    public int PlateCount { get => plateCount; }
+
+    public void AddPlate()
+    {
+        // TODO: select a random X and Y position on the surface of the table (add padding)
+        GameObject plateObj = new($"Plate {plateCount}");
+        plateObj.transform.position = Vector3.zero;
+
+        // add sprite renderer
+        var render = plateObj.AddComponent<SpriteRenderer>();
+        render.sprite = plateSprite;
+
+        // add plate
+        var plate = plateObj.AddComponent<Plate>();
+        plate.table = this;
+
+        GameObject foodObj = new("Food");
+        foodObj.transform.SetParent(plateObj.transform);
+        plateCount++;
+    }
+
+    public void RemovePlate(Plate plate)
+    {
+        Destroy(plate.gameObject);
+        plateCount--;
+    }
+
     // checks whether the position falls on the table
     public bool IsOnTable(Vector2 pos)
     {
-        // currently just assume false. implementing this later
+        // TODO: currently just assume false. implementing this later
         return false;
     }
 }
