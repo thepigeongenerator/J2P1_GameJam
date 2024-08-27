f849060623cfd03464a047b1c0d72f41ee42e191
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-08-27 22:54:37 +0200

    fix compilation errors (sorry)

--- Assets/scripts/Plate.cs
+++ Assets/scripts/Plate.cs
@@ -1,11 +1,12 @@
+using System;
 using UnityEngine;
 public class Plate : MonoBehaviour
 {
     // load in the plate sprites in the order from empty to full
     [SerializeField] private Sprite[] sprites;
-    private SpriteRenderer renderer;
+    private SpriteRenderer render;
     private Table table;
-    private uint plateFullness; // index of the sprite + indicator of how full the plate is
+    private int plateFullness; // index of the sprite + indicator of how full the plate is
     private float radius;
 
 
@@ -24,21 +25,21 @@ public class Plate : MonoBehaviour
 
         //otherwise decrease the fullness and update the sprite
         plateFullness--;
-        renderer.sprite = sprites[plateFullness];
+        render.sprite = sprites[plateFullness];
     }
 
     // checks whether a position falls on the plate
     private bool IsOnPlate(Vector2 pos)
     {
         // get the relative position as a non-negative
-        Vector2 rpos = transform.position - pos;
+        Vector2 rpos = new(transform.position.x - pos.x, transform.position.y - pos.y);
         rpos.x = Math.Abs(rpos.x);
         rpos.y = Math.Abs(rpos.y);
 
         // normalize a copy which will be a point on a circle
         Vector2 platePos = rpos;
         platePos.Normalize();
-        rpos * radius;
+        rpos *= radius;
 
         return rpos.x < platePos.x && rpos.y < platePos.y;
     }
@@ -47,20 +48,20 @@ public class Plate : MonoBehaviour
     private void Awake()
     {
         table = FindObjectOfType<Table>();
-        renderer = GetComponent<SpriteRenderer>();
-        plateFullness = sprites.length - 1; // set fullness to full
+        render = GetComponent<SpriteRenderer>();
+        plateFullness = sprites.Length - 1; // set fullness to full
 
         // update the renderer and calculate the radius (assuming all sprites are the same size)
-        renderer.sprite = renderer.sprite = sprites[plateFullness];
-        radius = renderer.size.y / 2.0F;
+        render.sprite = render.sprite = sprites[plateFullness];
+        radius = render.size.y / 2.0F;
     }
 
     // called on every frame upate
     private void Update()
     {
-        if (Input.GetMouseButton(MouseButton.LeftMouse))
+        if (Input.GetMouseButton(0))
         {
-            bool isHeld = Input.GetMouseButtonDown(MouseButton.LeftMouse) == false;
+            bool isHeld = Input.GetMouseButtonDown(0) == false;
             OnMouse(isHeld);
         }
         else if (table.IsOnTable(transform.position)) {}
