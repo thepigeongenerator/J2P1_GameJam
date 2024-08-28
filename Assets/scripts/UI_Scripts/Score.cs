e03acb4ce75c0d8b28c0c11bae812e8118301a4b
LeonBijker <0351849@student.rocvantwente.nl>
2024-08-28 12:57:53 +0200

    misc
    dingen uigeprobeerd en uiteindelijk een method gemaakt dat wordt gerunt wanneer  de plate weg gaat

--- Assets/scripts/UI_Scripts/Score.cs
+++ Assets/scripts/UI_Scripts/Score.cs
@@ -9,31 +9,21 @@ public class Score : MonoBehaviour
 {
     [SerializeField] TMP_Text score;
     private int scoreInt;
-    GameObject food;
-    bool FoodDeleted;
     // Start is called before the first frame update
     void Start()
     {
-        food = GameObject.Find("Food");
-        scoreInt = -1;
-        FoodDeleted = false;
-        if(food != null)
-        {
-
-        }
+        scoreInt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(food != null && FoodDeleted == false)
-        {
-            FoodDeleted=true;
-            scoreInt++;
-            FoodDeleted = false;
-        }
+      
         score.text = "score:" + scoreInt.ToString();
         PlayerPrefs.SetInt("Scoreint", scoreInt);
+    }
+   public void ScorePlus()
+    {
+        scoreInt++;
     }
 }
